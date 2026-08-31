69ce9d044498f27be80e1ff1d4052290e786fa19
fahadiqbal-01 <fahad.iqbal.im.47@gmail.com>
2026-08-31 23:25:09 +0600

    useless files deleted

--- main.py
+++ main.py
@@ -19,15 +19,10 @@ app.include_router(photopostrouter)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost:3001",
         "http://127.0.0.1:3001",
-        "http://localhost:3002",
         "http://127.0.0.1:3002",
-        "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:8000",
         "http://127.0.0.1:8000",
     ],
     allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:[0-9]+)?$",
