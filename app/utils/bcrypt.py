80df3817211463247dbe3304f0cb019d4f370dc7
fahadiqbal-01 <fahad.iqbal.im.47@gmail.com>
2026-09-11 01:18:28 +0600

    salt decreased

--- app/utils/bcrypt.py
+++ app/utils/bcrypt.py
@@ -2,7 +2,7 @@ import bcrypt
 
 def hash_password(password: str) -> str:
     password_bytes = password.encode('utf-8')
-    salt = bcrypt.gensalt(rounds=12)
+    salt = bcrypt.gensalt(rounds=10)
     hashed = bcrypt.hashpw(password_bytes, salt)
     return hashed.decode('utf-8')
 
